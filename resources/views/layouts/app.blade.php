70b139a4ee65a0b73915746ee51caa81bed3f5ab
Ramsey <rdarling42@gmail.com>
2016-05-17 13:51:11 -0400

    increasing font size a bit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,6 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -76,6 +75,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/register') }}">Register Admin</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
